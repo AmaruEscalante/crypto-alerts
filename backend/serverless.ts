81bcabd2223b05465602df6f0655825c01207199
AmaruEscalante <amaruescalante@gmail.com>
2021-10-21 03:29:06 -0500

    feat: add configurations and packages for environment

--- backend/serverless.ts
+++ backend/serverless.ts
@@ -1,5 +1,5 @@
 import type { AWS } from "@serverless/typescript";
-
+import createAlertModel from "./models/create-alert-model";
 // import hello from "@functions/hello";
 
 const serverlessConfiguration: AWS = {
@@ -15,12 +15,28 @@ const serverlessConfiguration: AWS = {
       define: { "require.resolve": undefined },
       platform: "node",
     },
+    "serverless-offline": {
+      port: 3003,
+    },
+    dynamodb: {
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      },
+      stages: ["dev", "prod"],
+    },
   },
-  plugins: ["serverless-esbuild", "serverless-offline"],
+  plugins: [
+    "serverless-esbuild",
+    "serverless-offline",
+    "serverless-dynamodb-local",
+    "serverless-iam-roles-per-function",
+  ],
   provider: {
     name: "aws",
     region: "us-east-1",
-    stage: "dev",
+    stage: process.env.stage || "dev",
     runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
@@ -29,14 +45,61 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      // ALERTS_TABLE: `${process.env.ALERTS_TABLE}-${process.env.stage || "dev"}`,
+      ALERTS_TABLE: "alerts-dev",
     },
     lambdaHashingVersion: "20201221",
+    tracing: {
+      lambda: true,
+      apiGateway: true,
+    },
+    logs: {
+      restApi: true,
+    },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: "Allow",
+            Action: ["xray:PutTelemtryRecords", "xray:PutTraceSegments"],
+            Resource: "*",
+          },
+        ],
+      },
+    },
   },
   // import the function via paths
   functions: {
     checkPrices: {
       handler: "./src/lambda/jobs/checkPrices.handler",
       events: [{ schedule: "rate(1 minute)" }],
+    },
+    createAlert: {
+      handler: "./src/lambda/http/createAlert.handler",
+      events: [
+        {
+          http: {
+            method: "post",
+            path: "alert",
+            cors: true,
+            request: {
+              schema: {
+                "application/json": createAlertModel,
+              },
+            },
+          },
+        },
+      ],
+      // @ts-expect-error: Let's ignore a single compiler
+      iamRoleStatements: [
+        {
+          Effect: "Allow",
+          Action: ["dynamodb:PutItem"],
+          Resource: [
+            "arn:aws:dynamodb:*:*:table/${self:provider.environment.ALERTS_TABLE}",
+          ],
+        },
+      ],
     },
   },
 
@@ -45,7 +108,7 @@ const serverlessConfiguration: AWS = {
       AlertsTable: {
         Type: "AWS::DynamoDB::Table",
         Properties: {
-          TableName: "Alerts",
+          TableName: "${self:provider.environment.ALERTS_TABLE}",
           AttributeDefinitions: [
             {
               AttributeName: "userId",
