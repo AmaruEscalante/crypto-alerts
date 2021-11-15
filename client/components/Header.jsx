9ee6889e810c57871e83454842dcafb5f7b50a0c
AmaruEscalante <amaruescalante@gmail.com>
2021-11-15 12:53:00 -0500

    feat(auth): add authentication with Auth0

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-const Header = () => {
+const Header = (user) => {
   return (
     <div className="bg-[#131517] sticky top-0 z-50">
       <div className="flex justify-between items-center max-w-6xl mx-5 xl:mx-auto">
@@ -13,8 +13,8 @@ const Header = () => {
             height="40"
             width="40"
           />
-          <h1 className="flex hidden font-logo text-yellow-300 text-xl ml-2 lg:inline-grid cursor-pointer">
-            THE CRYPTO WAY
+          <h1 className="hidden font-logo text-yellow-300 text-xl ml-2 lg:inline-grid cursor-pointer">
+            THE CRYPTO BAY
           </h1>
         </div>
         {/* Search bar */}
@@ -27,8 +27,26 @@ const Header = () => {
         </div>
         <div className="flex ml-5">
           {/* User */}
-          <div></div>
-          <h3 className="text-white text-xl">Amaru Escalante</h3>
+          {/* <div></div> */}
+          {console.log("USER is", user)}
+          {/* {console.log(user.user)} */}
+          {user?.user?.name !== undefined ? (
+            <div className="text-white text-xl">
+              Welcome {user.user.given_name}!
+              <a
+                className="ml-5 bg-red-900 p-3 rounded-md hover:bg-red-500"
+                href="/api/auth/logout"
+              >
+                Logout
+              </a>
+            </div>
+          ) : (
+            // Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+            // <h3 className="text-white text-xl"></h3>
+            <a className="text-white text-xl" href="/api/auth/login">
+              Login
+            </a>
+          )}
           {/* Notifications */}
         </div>
       </div>
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,16 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Feed from "../components/Feed";
+import { useUser } from "@auth0/nextjs-auth0";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+  console.log(`User is : ${user}`);
+
   return (
     <div className="bg-secondary" h-screen overflow-y-scroll>
       <Head>
-        <title>The Crypto Way</title>
+        <title>The Crypto Bay - TCB</title>
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Iceberg&display=swap"
@@ -18,7 +22,7 @@ export default function Home() {
         ></link>
       </Head>
       {/* Header */}
-      <Header />
+      <Header user={user} />
       <Feed />
     </div>
   );
